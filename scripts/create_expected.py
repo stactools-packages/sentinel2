176d53a2de538cb24ae370e1d81240d9a75e313e
Phil Varner <pvarner@element84.com>
2024-04-03 16:22:23 -0400

    Added a 'raster:bands' field to the 'visual' asset (TCI). (#170)
    * Added a 'raster:bands' field to the 'visual' asset (TCI).
    
    * install requests for tests

--- scripts/create_expected.py
+++ scripts/create_expected.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from stactools.sentinel2 import stac
 
 EXCLUDE = [
+    "S2A_T60CWS_20240109T203651_L2A-pole-and-antimeridian-bad-geometry-after-reprojection",
+    "S2B_OPER_MSI_L2A_DS_VGS1_20201101T095401_S20201101T074429-no-data",
+    "S2A_OPER_MSI_L2A_DS_2APS_20230105T201055_S20230105T163809",
     "S2A_MSIL2A_20150826T185436_N0212_R070_T11SLT_20210412T023147",
     "S2A_MSIL2A_20180721T053721_N0212_R062_T43MDV_20201011T181419.SAFE",
     "S2A_MSIL2A_20190212T192651_N0212_R013_T07HFE_20201007T160857",
@@ -19,6 +22,7 @@ data_files = root / "tests" / "data-files"
 for path in data_files.iterdir():
     if path.name in EXCLUDE or path.name.startswith("."):
         continue
+    print(path)
     item = stac.create_item(str(path))
     item.set_self_href(str(data_files / path.name / "expected_output.json"))
     item.make_asset_hrefs_relative()
